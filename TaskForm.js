ecab34f66d890340f07e3ea31bfc415689f427c9
Dadi Atar <atardadi@gmail.com>
2016-03-18 09:26:17 +0200

    website broken on mobile

---
+++ TaskForm.js
@@ -0,0 +1,84 @@
+import React  from 'react-native';
+
+
+const {
+  View,
+  TouchableHighlight,
+  Text,
+  TextInput
+} = React;
+
+const styles = React.StyleSheet.create({
+  container: {
+    paddingTop: 150,
+    backgroundColor: '#F7F7F7',
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#D7D7D7', 
+    height: 50,
+    marginLeft: 10,
+    padding: 15,
+    borderRadius: 3
+  },
+  button: {
+    height: 45,
+    alignSelf: 'stretch',
+    margin: 10,
+    backgroundColor: '#05A5D1',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#FAFAFA',
+    fontSize: 18,
+    fontWeight: '600'
+  },
+  cancelButton: {
+    backgroundColor: '#666'
+  }
+});
+
+class TaskForm extends React.Component {
+  constructor(props, context){
+    super(props,context);
+   
+  }
+
+  onChange(text) {
+    this.task = text;
+  }
+
+  onAddPresses() {
+    this.props.onAdd(this.task);
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.input}
+          onChangeText={this.onChange.bind(this)}
+          />
+        <TouchableHighlight style={styles.button}
+          onPress={this.onAddPresses.bind(this)}>
+          <Text style={styles.buttonText}>Add</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={[styles.button, styles.cancelButton]}
+          onPress={this.props.onCancel}>
+          <Text style={styles.buttonText}>Cancel</Text>
+        </TouchableHighlight>
+      </View>
+      
+    );
+  }
+}
+
+TaskForm.propTypes = {
+  onCancel: React.PropTypes.func.isRequired,
+  onAdd: React.PropTypes.func.isRequired,
+};
+
+
+export default TaskForm;
---
+++ TaskList.js
@@ -0,0 +1,83 @@
+import React  from 'react-native';
+import TaskRow from './TaskRow';
+
+const {
+  View,
+  ListView,
+  TouchableHighlight,
+  Text
+} = React;
+
+const styles = React.StyleSheet.create({
+  container: {
+    paddingTop: 40,
+    backgroundColor: '#D4D4D4',
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  button: {
+    height: 60,
+    borderColor: '#05A5D1',
+    borderWidth: 2,
+    margin: 20,
+    backgroundColor: '#333',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: '#FAFAFA',
+    fontSize: 20,
+    fontWeight: '600'
+  }  
+});
+
+class TaskList extends React.Component {
+  constructor(props, context){
+    super(props,context);
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1,r2) => r1 !== r2
+    });
+
+    this.state = {
+      dataSource: ds.cloneWithRows(props.todos)
+    };
+  }
+
+  renderRow(todo) {
+    return (
+      <TaskRow todo={todo}></TaskRow>
+    );
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const dataSource = this.state.dataSource.cloneWithRows(nextProps.todos);
+    this.setState({ dataSource });
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <ListView 
+          dataSource={this.state.dataSource}
+          key={this.props.todos}
+          renderRow={this.renderRow.bind(this)}
+          />
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.props.onAddStarted}>
+          <Text style={styles.buttonText}>Add one</Text>
+        </TouchableHighlight>
+      </View>
+      
+    );
+  }
+}
+
+TaskList.propTypes = {
+  todos: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
+  onAddStarted: React.PropTypes.func.isRequired,
+};
+
+
+export default TaskList;
