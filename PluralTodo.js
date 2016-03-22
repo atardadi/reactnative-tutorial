e4fcf2194364c5937d3e77de30c254b30534f35f
Dadi Atar <atardadi@gmail.com>
2016-03-22 20:38:29 +0200

    course done

--- PluralTodo.js
+++ PluralTodo.js
@@ -8,21 +8,18 @@ let {
 } = React;
 import TaskList from './TaskList';
 import TaskForm from './TaskForm';
+import store from './todoStore';
 
 class PluralTodo extends Component {
   constructor(props, context) {
     super(props, context);
-    this.state = {
-      todos: [
-        {
-          task: 'Learn React Native'
-        },
-        {
-          task: 'Learn Redux'
-        }
-      ]
-    };
+    this.state = store.getState();
+    
+    store.subscribe(() => {
+      this.setState(store.getState()); // eslint-diasble-line react/no-set-state
+    });
   }
+
   onAddStarted() {
     this.nav.push({
       name: 'taskform'
@@ -35,33 +32,46 @@ class PluralTodo extends Component {
 
   onAdd(task) {
     console.log('the task', task);
-    this.state.todos.push({task});
-    this.setState({ todos: this.state.todos });
+    
+    store.dispatch({
+      type: 'ADD_TODO',
+      task,
+    });
+
     this.nav.pop();
   }
 
   onDone(todo) {
     console.log('Completed',todo);
-    const filteredTodos = 
-      this.state.todos.filter((filterTodo) => {
-        return filterTodo !== todo;
-      });
+    store.dispatch({
+      type: 'DONE_TODO',
+      todo,
+    });
+  }
 
-    this.setState({todos: filteredTodos});
+  onToggle() {
+    store.dispatch({
+      type: 'TOGGLE_STATE',
+    });
   }
 
   renderScene(route, nav) {
     switch (route.name) {
       case 'taskform':
-        return <TaskForm
-          onAdd={this.onAdd.bind(this) }
-          onCancel={this.onCancel.bind(this)}/> 
+        return (
+          <TaskForm
+            onAdd={this.onAdd.bind(this)}
+            onCancel={this.onCancel.bind(this)}
+          />
+        );
       default:
         return (
           <TaskList
             onAddStarted={this.onAddStarted.bind(this)}
             onDone={this.onDone.bind(this)}
+            onToggle={this.onToggle.bind(this)} 
             todos={this.state.todos}
+            filter={this.state.filter}
           />
         );
     }
